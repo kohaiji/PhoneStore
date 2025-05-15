5b1dcef8795e428d61c4d738c3686f7888fb12ba
Giang Nguyễn <ntg1611@gmail.com>
2025-05-16 00:51:01 +0700

    big update

--- resources/views/admin/adminIndex.blade.php
+++ resources/views/admin/adminIndex.blade.php
@@ -57,7 +57,7 @@
         <!-- Sale & Revenue Start -->
         <div class="container-fluid pt-4 px-4">
             <div class="row g-4">
-                <h4>Welcome!!</h4>
+                <h4>Welcome Admin {{\Illuminate\Support\Facades\Auth::user()->name}}</h4>
 {{--                <div class="col-sm-6 col-xl-3">--}}
 {{--                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">--}}
 {{--                        <i class="fa fa-chart-line fa-3x text-primary"></i>--}}
