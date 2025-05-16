5b795d167dd4bf76a04a79583529757567e6a18e
Giang Nguyễn <ntg1611@gmail.com>
2025-05-16 21:10:46 +0700

    update navbar

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -41,7 +41,7 @@
                     <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                         <div class="user-menu d-flex">
                             <div class="user-name text-end me-3">
-                                <h6 class="mb-0 text-gray-600">John Ducky</h6>
+                                <h6 class="mb-0 text-gray-600">{{\Illuminate\Support\Facades\Auth::user()->name}}</h6>
                                 <p class="mb-0 text-sm text-gray-600">Administrator</p>
                             </div>
                             <div class="user-img d-flex align-items-center">
@@ -53,7 +53,7 @@
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
                         <li>
-                            <h6 class="dropdown-header">Hello, John!</h6>
+                            <h6 class="dropdown-header">Hello, {{\Illuminate\Support\Facades\Auth::user()->name}}!</h6>
                         </li>
                         <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
                                 Profile</a></li>
@@ -64,7 +64,7 @@
                         <li>
                             <hr class="dropdown-divider">
                         </li>
-                        <li><a class="dropdown-item" href="#"><i
+                        <li><a class="dropdown-item" href="/signout"><i
                                     class="icon-mid bi bi-box-arrow-left me-2"></i> Logout</a></li>
                     </ul>
                 </div>
