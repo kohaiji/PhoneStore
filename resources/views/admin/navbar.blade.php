447073aa854c2e60167b6536b6f778e38e5072ee
Giang Nguyễn <ntg1611@gmail.com>
2025-06-18 01:03:29 +0700

    update admin navbar

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -56,10 +56,10 @@
                         <li>
                             <h6 class="dropdown-header">Hello, {{\Illuminate\Support\Facades\Auth::user()->name}}!</h6>
                         </li>
-                        <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My
-                                Profile</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-gear me-2"></i>
-                                Settings</a></li>
+{{--                        <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-person me-2"></i> My--}}
+{{--                                Profile</a></li>--}}
+{{--                        <li><a class="dropdown-item" href="#"><i class="icon-mid bi bi-gear me-2"></i>--}}
+{{--                                Settings</a></li>--}}
                         <li><a class="dropdown-item" href="/ClientIndex"><i class="icon-mid bi bi-wallet me-2"></i>
                                 Go to Client</a></li>
                         <li>
